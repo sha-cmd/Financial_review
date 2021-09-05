4bd3cc19b8e011968f319d87f62cc86a7f6f0631
romain Boyrie <romain@boyrie.email>
2021-09-05 17:11:58 +0200

    Ajout des checker et ticket

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,6 +1,7 @@
 __all__ = ['db_conn', 'db_exist', 'logger', 'log_init',
            'connexion', 'write_to_db', 'close_connexion',
-           'CreateTicket', 'DownloadTicket']
+           'CreateTicket', 'DownloadTicket', 'ReadTicket',
+           'Checker', 'Ticket']
 
 from .db_conn import db_exist
 from .db_conn import connexion
@@ -11,3 +12,5 @@ from .logger import log_init
 from .CreateTicket import CreateTicketGroup
 from .DownloadTicket import DownloadTicket
 from .ReadTicket import ReadTicket
+from .Checker import Checker
+from .Ticket import Ticket
