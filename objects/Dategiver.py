e9314074bdf72f9e19ce4d2a360254216bac1dbf
romain Boyrie <romain@boyrie.email>
2021-09-11 17:45:07 +0200

    Considération de la classe yfinance downloader.

--- objects/Dategiver.py
+++ objects/Dategiver.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from objects.Checker import Checker
 from objects.Clock import Clock
+from objects.Ticket import DATE_DEBUT
 from objects.logger import log_init as log
 logger = log()
 
@@ -9,10 +10,16 @@ logger = log()
 class Dategiver:
 
     def __init__(self, checker: Checker, clock: Clock):
-        self.conclusion: bool = not (clock.last_day == np.datetime64(checker.date_end))
-        if self.conclusion:
-            self.from_date = np.datetime64(checker.date_end)
+        self.checker = checker
+        if checker.is_in_db:
+            self.conclusion: bool = not (clock.last_day == np.datetime64(checker.date_end))
+            if self.conclusion:
+                self.from_date = np.busday_offset(np.datetime64(checker.date_end), 1)
+                self.to_date = clock.last_day
+        else:
+            self.from_date = DATE_DEBUT
             self.to_date = clock.last_day
+            self.conclusion = False
 
     def __str__(self):
-        return f"Il y a besoin de mise à jour {self.conclusion}"
+        return f"Il y a besoin de mise à jour {self.conclusion}, est dans la base {self.checker.is_in_db}"
