4f97df812fdbab1e1c945a39a18ea9f8e923317d
romain Boyrie <romain@boyrie.email>
2021-09-11 17:40:54 +0200

    Indentification de l'objet

--- objects/Ticket.py
+++ objects/Ticket.py
@@ -1,8 +1,13 @@
 
+from data.securities import liste_complete
 from objects.logger import log_init as log
+
+DATE_DEBUT = '1983-01-01'
 logger = log()
 
 class Ticket:
 
     def __init__(self, name: str):
-        self.name = name
+        self.name = name.upper()
+        self.mnemo = liste_complete()[1][self.name]
+        self.name_table = self.name.lower().replace(' ', '_')
