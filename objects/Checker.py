b946c3efc740bec65f1fb04b4de68b4560334a3c
romain Boyrie <romain@boyrie.email>
2021-09-11 17:45:35 +0200

    Renommage

--- objects/Checker.py
+++ objects/Checker.py
@@ -1,5 +1,5 @@
 
-from objects.ReadTicket import ReadTicket
+from objects.Reader import Reader
 from objects.Ticket import Ticket
 from objects.logger import log_init as log
 logger = log()
@@ -8,7 +8,7 @@ logger = log()
 class Checker:
 
     def __init__(self, ticket: Ticket):
-        self.is_in_db, self.date_begin, self.date_end = ReadTicket(ticket).execute()
+        self.is_in_db, self.date_begin, self.date_end = Reader(ticket).execute()
         self.check: list = [x.split()[0] for x in self.__str__().split('->')[1::2]]
         self.check[0] = bool(self.check[0])
 
