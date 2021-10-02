5bfe16dfa10b6ca68555c855928e2bb5a4dcbf45
romain Boyrie <romain@boyrie.email>
2021-10-02 18:21:28 +0200

    Présentation de la fonction.

--- objects/Reader.py
+++ objects/Reader.py
@@ -1,3 +1,8 @@
+"""
+Cette classe execute une fonction execute() qui tente de trouver le ticket dans la base de données.
+Elle peut néanmoins rapporter le cadre de données par une sous-fonction de lecture, une fois la table contrôlée,
+par une fonction read().
+"""
 import yfinance as yf
 import pandas as pd
 import numpy as np
@@ -12,10 +17,11 @@ from .db_conn import connexion
 from .db_conn import write_to_db
 from .db_conn import close_connexion
 from data.securities import liste_complete as lst_cplt
-from objects.Ticket import  DATE_DEBUT
+from objects.Ticket import DATE_DEBUT
 from objects.Clock import Clock
 
 logger = log_init()
+
 
 class Reader:
 
@@ -30,12 +36,12 @@ class Reader:
 
         try:
             df = pd.read_sql_query("SELECT * FROM " + self.table + suffix, conn, index_col='date')
-            return(True, str(df.index[0]).split(' ')[0], str(df.index[-1]).split(' ')[0])
+            return True, str(df.index[0]).split(' ')[0], str(df.index[-1]).split(' ')[0]
 
         except (OperationalError, DatabaseError) as e:
             logger.debug(self.name + ' n\'est pas dans la table')
             print(e, 'Class Reader')
-            return(False, DATE_DEBUT, Clock().last_day)
+            return False, DATE_DEBUT, Clock().last_day
 
     def read(self):
         conn = connexion()
