ad34ec3d89b50c81195c50310b8bebf9f29bd53b
romain Boyrie <romain@boyrie.email>
2021-10-02 23:47:46 +0200

    Organization of supply chain.

--- objects/FetchAndSave.py
+++ objects/FetchAndSave.py
@@ -5,6 +5,8 @@ Cette classe rapporte et sauvegarde les données en base.
 from objects.Writer import Writer
 from objects.Ticket import Ticket
 from objects.Downloader import Downloader
+from objects.Checker import Checker
+from data.securities import liste_complete
 
 
 class FetchAndSave:
@@ -15,4 +17,13 @@ class FetchAndSave:
         self.sharpen()
         Writer(self.downloader)
 
+    @staticmethod
+    def download_securities():
+        for name, value in liste_complete()[1].items():
 
+            t = Ticket(name)
+            c = Checker(t)
+
+            if not c.is_in_db:
+                FetchAndSave(name)
+                print(t.name_table + " télécharger et sauvegarder")
