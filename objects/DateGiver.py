139254794ccb360af38d80db3eeb392e7968f577
romain Boyrie <romain@boyrie.email>
2021-10-02 23:10:13 +0200

    Logically optimized

--- objects/DateGiver.py
+++ objects/DateGiver.py
@@ -16,7 +16,7 @@ class DateGiver:
     def __init__(self, checker: Checker, clock: Clock):
         self.checker = checker
         if checker.is_in_db:
-            self.conclusion: bool = not (clock.last_day == np.datetime64(checker.date_end))
+            self.conclusion: bool = np.datetime64(clock.last_day) > np.datetime64(checker.date_end)
             if self.conclusion:
                 self.from_date = np.busday_offset(np.datetime64(checker.date_end), 1)
                 self.to_date = clock.last_day
@@ -26,4 +26,4 @@ class DateGiver:
             self.conclusion = False
 
     def __str__(self):
-        return f"Il y a besoin de mise à jour {self.conclusion}, est dans la base {self.checker.is_in_db}"
+        return f"Il y a besoin de mise à jour : {self.conclusion}, est dans la base : {self.checker.is_in_db}"
