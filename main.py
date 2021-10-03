3685a14cebd83e574a95e940f28f560ddccdf8f3
romain Boyrie <romain@boyrie.email>
2021-10-03 05:54:46 +0200

    More Functionality

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 Ce projet permet de prendre des décisions pour les décideurs, sur le marché financier.
 """
 from objects.Update import Update
-
+from objects.Analyse import Analyse
 
 def main():
-    Update('ATOS')
-
+   # Update('ATOS')
+    Analyse().to_xlsx()
 
 if __name__ == '__main__':
     main()
