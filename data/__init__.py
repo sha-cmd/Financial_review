4f7e685e46c1784afc9ad7acc145c2d091e5fd78
Romain Boyrie <romain@boyrie.email>
2024-08-30 18:22:09 +0200

    Utilisation du nom data

--- data/__init__.py
+++ data/__init__.py
@@ -3,10 +3,10 @@ __all__ = ['securities', 'liste_indices', 'liste_actions_pme',
            'liste_actions_usa', 'print_actions_usa',
            'liste_complete']
 
-from .securities import liste_indices
-from .securities import liste_actions_pme
-from .securities import print_indices
-from .securities import print_actions_pme
-from .securities import liste_actions_usa
-from .securities import print_actions_usa
-from .securities import liste_complete
+from data.securities import liste_indices
+from data.securities import liste_actions_pme
+from data.securities import print_indices
+from data.securities import print_actions_pme
+from data.securities import liste_actions_usa
+from data.securities import print_actions_usa
+from data.securities import liste_complete
