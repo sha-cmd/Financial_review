686ed64fdd8bf17c51a45d26fa8cfcb0a8474911
Romain Boyrie <romain@boyrie.email>
2024-08-30 18:50:12 +0200

    Ajout de commentaire d’aide, création de la base

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 """
 Ce projet permet de prendre des décisions pour les décideurs, sur le marché financier.
 """
+from multiprocessing import Process
 from objects.Downloader import Downloader
 from objects.Update import Update
 from objects.Analyse import Analyse
 from objects.Ticket import Ticket
 from objects.Report import Report
 from objects.API_request import API_request
+from os.path import exists
 from data.securities import liste_complete
 
 
 def create_db():
+    file_exists = exists('data/financial_db.sqlite')
+    if not file_exists:
+        con = sqlite3.connect("data/financial_db.sqlite")
     for key, value in liste_complete()[1].items():
         print(key)
         ticket = Ticket(key)
-        Downloader(ticket)
+        p = Process(target=Downloader, args=[ticket])
+        p.run()
 
 def maj_db():
     for key, value in liste_complete()[1].items():
         print(key)
-        Update(key)
+        p = Process(target=Update, args=[key])
+        p.run()
 
 
 def excel_report():
@@ -36,15 +43,17 @@ def pdf_report():
 
 def api_report():
     a = API_request()
-    a.to_xlsx(6)  # 2 et 4 et 6
+    a.to_xlsx(2)  # 2 et 4 et 8 et 10
+    a.to_xlsx(4)  # 2 et 4 et 8 et 10
+    a.to_xlsx(10)  # 2 et 4 et 8 et 10
 
 def main():
-    #create_db()
+    create_db()  # Pour recréer une base avec 10 ans de données
     #maj_db()
-    #text_report()
+    #text_report()  # Select portfolio in data/selection.txt
     #excel_report()
     #pdf_report()
-    api_report()
+    #api_report()  # Need an API KEY to https://site.financialmodelingprep.com/developer/docs/pricing
 
 if __name__ == '__main__':
     main()
